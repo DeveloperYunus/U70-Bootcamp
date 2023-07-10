4c3a501e3471309d8a697e75125c765d7bf016f9
Yunus Emre <113380693+DeveloperYunus@users.noreply.github.com>
2023-07-10 22:42:56 +0300

    Tower Isle
    - Enemy skeleton added the tower isle
    - Collectable bullet and food added tower isle
    - Ship move foam vfx added

--- Assets/-U70/Yunus/Scripts/Enemy/EnemyProducer.cs
+++ Assets/-U70/Yunus/Scripts/Enemy/EnemyProducer.cs
@@ -1,20 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyProducer : MonoBehaviour
 {
     public GameObject enemy;
-    public float timer;
+
+    [Header("--- Skeleton Stats ---")]
+    public float health;
+    public float armour;
+    public float damage;
+
+    [Header("--- Skeleton Spawn ---")]
+    public int skeletonAmount;
+    public float range;
 
     void Start()
     {
-        InvokeRepeating(nameof(ProduceEnemy), 0, timer);
+        ProduceEnemy(skeletonAmount);
     }
-    void ProduceEnemy()
+    void ProduceEnemy(int amount)
     {
-        GameObject a = Instantiate(enemy, transform.position, Quaternion.identity, transform);
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject a = Instantiate(enemy, transform.position, Quaternion.identity, transform);
+            a.transform.position = new Vector3(transform.position.x + Random.Range(-range, range), transform.position.y, transform.position.z + Random.Range(-range, range));
 
-        a.transform.position = new Vector3(transform.position.x + Random.Range(-5, 5), transform.position.y, transform.position.z + Random.Range(-5, 5));
+            a.GetComponent<EnemyHP>().SetSkeletonStats(health,armour,damage);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).GetComponent<EnemyNavMesh>().FollowPlayer();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(range, 0, range));
     }
 }
